8060ede5da60ae928a87f73329abd8237ba49545
Svetlana Linuxenko <linuxenko@yahoo.com>
2017-01-22 21:51:44 +0200

    spatial navigation tests

--- tests/spatial-init.test.js
+++ tests/spatial-init.test.js
@@ -2,6 +2,16 @@ var expect = require('chai').expect;
 var SN = require('../lib/spatial');
 
 describe('Spatial tests', function () {
+  function createElement (left, top) {
+    var element = document.createElement('div');
+    element.style.position = 'absolute';
+    element.style.width = element.style.height = '20px';
+    element.style.top = top + 'px';
+    element.style.left = left + 'px';
+
+    return element;
+  }
+
   it('should initialize SpatialNavigator', function () {
     expect(SN).to.be.a('function');
     var sp = new SN();
@@ -46,5 +56,31 @@ describe('Spatial tests', function () {
     expect(sn._focus).to.be.equal(elements[0]);
 
     elements.map(function (e) { document.body.removeChild(e); });
+  });
+
+  it('should navigate inside of layout', function () {
+    var elements = [
+      createElement(0, 0),
+      createElement(0, 30),
+      createElement(0, 60),
+      createElement(30, 30),
+      createElement(30, 60),
+      createElement(30, 90),
+      createElement(60, 60)
+    ].map(function (e) { return document.body.appendChild(e); });
+
+    var sn = new SN(elements);
+    sn.focus();
+
+    expect(sn._focus).to.be.equal(elements[0]);
+    sn.move('right');
+    expect(sn._focus).to.be.equal(elements[3]);
+    sn.move('down');
+    expect(sn._focus).to.be.equal(elements[4]);
+    sn.move('right');
+    expect(sn._focus).to.be.equal(elements[6]);
+    sn.move('left');
+    sn.move('left');
+    expect(sn._focus).to.be.equal(elements[2]);
   });
 });
