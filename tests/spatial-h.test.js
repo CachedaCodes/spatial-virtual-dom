47801c63c512042123b6db04f87160f82956df1a
Svetlana Linuxenko <linuxenko@yahoo.com>
2017-01-24 17:50:29 +0200

    focus/unfocus node events

--- tests/spatial-h.test.js
+++ tests/spatial-h.test.js
@@ -2,6 +2,9 @@ var expect = require('chai').expect;
 var diff = require('../').diff;
 var patch = require('../').patch;
 
+window.jQuery = window.$ = require('jquery');
+var syn = require('syn');
+
 var keys = require('./fixtures/keys');
 
 describe('H spatial tests', function () {
@@ -41,11 +44,75 @@ describe('H spatial tests', function () {
     expect(c.sn._focus).to.be.null;
   });
 
-  it('should handle general keys', function () {
+  it('should focus item on click', function (done) {
     var h = require('../').spatial({ autofocus: true, keys: keys });
     var c = h('div', null, h('div', { focusable: true }, 'one'), h('div', { focusable: true }, 'two'));
 
     var dom = c.render();
 
+    c.sn.unfocus();
+    document.body.appendChild(dom);
+    expect(c.sn._focus).to.be.null;
+    syn.click(dom.childNodes[0], function () {
+      expect(c.sn._focus).not.to.be.null;
+      done();
+    });
+  });
+
+  it('should focus item by navigating keys', function (done) {
+    var h = require('../').spatial({ autofocus: false, keys: keys });
+    var c = h('div', null, h('div', { focusable: true }, 'one'), h('div', { focusable: true }, 'two'));
+
+    var dom = c.render();
+
+    document.body.appendChild(dom);
+    syn.type(dom, '[down]', function () {
+      syn.type(dom, '[down]', function () {
+        expect(c.sn._focus).to.be.equal(dom.childNodes[1]);
+        done();
+      });
+    });
+  });
+
+  it('should fire focus events', function (done) {
+    var h = require('../').spatial({ autofocus: true, keys: keys });
+    var c = h('div', null, h('div', { focusable: true }, 'one'), h('div', { focusable: true }, 'two'));
+
+    var dom = c.render();
+    document.body.appendChild(dom);
+
+    dom.childNodes[1].addEventListener('onfocus', function () {
+      done();
+    });
+
+    syn.type(dom, '[down]');
+  });
+
+  it('should fire unfocus events', function (done) {
+    var h = require('../').spatial({ autofocus: true, keys: keys });
+    var c = h('div', null, h('div', { focusable: true }, 'one'), h('div', { focusable: true }, 'two'));
+
+    var dom = c.render();
+    document.body.appendChild(dom);
+
+    dom.childNodes[0].addEventListener('onunfocus', function () {
+      done();
+    });
+
+    syn.type(dom, '[down]');
+  });
+
+  it('should fire unfocus events focused node', function (done) {
+    var h = require('../').spatial({ autofocus: true, keys: keys });
+    var c = h('div', null, h('div', { focusable: true }, 'one'), h('div', { focusable: true }, 'two'));
+
+    var dom = c.render();
+    document.body.appendChild(dom);
+
+    dom.childNodes[0].addEventListener('onunfocus', function () {
+      done();
+    });
+
+    syn.type(dom.childNodes[0], '[down]');
   });
 });
